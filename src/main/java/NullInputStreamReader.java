c63489a66ccdb04958fd2f9cf9bbd5627d9a1047
annahosanna <Anna.Hosanna@gmail.com>
2024-02-07 01:18:32 -0500

    Update NullInputStreamReader.java

--- src/main/java/NullInputStreamReader.java
+++ src/main/java/NullInputStreamReader.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.Reader;
 
+// Do I actually need to read data from the stream in order to consume data.
 //  in = new BufferedReader(new InputStreamReader(conn.getInputStream(), charset));
 
 public class NullInputStreamReader extends Reader {
