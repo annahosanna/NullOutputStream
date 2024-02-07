cd06d8ee6106a38413677d8daeee4ed3fe891e25
annahosanna <Anna.Hosanna@gmail.com>
2024-02-07 01:38:35 -0500

    Update NullInputStreamReader.java

--- src/main/java/NullInputStreamReader.java
+++ src/main/java/NullInputStreamReader.java
@@ -2,7 +2,12 @@ import java.io.IOException;
 import java.io.Reader;
 
 // Do I actually need to read data from the stream in order to consume data.
-//  in = new BufferedReader(new InputStreamReader(conn.getInputStream(), charset));
+// in = new BufferedReader(new InputStreamReader(conn.getInputStream(), charset));
+
+// Based on the output stream requirements:
+// If cbuf is null, a NullPointerException is thrown.
+// If off is negative, or len is negative, or off+len is greater than the length of the array cbuf, then an IndexOutOfBoundsException is thrown.
+// However if any of those conditions are true -1 is probably ok
 
 public class NullInputStreamReader extends Reader {
 
