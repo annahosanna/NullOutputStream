e305867cb607466a6c5edd61989a37f8829bc2bd
annahosanna <Anna.Hosanna@gmail.com>
2024-02-08 23:09:27 -0500

    Update DiscardInputStream.java

--- src/main/java/DiscardInputStream.java
+++ src/main/java/DiscardInputStream.java
@@ -2,11 +2,11 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class DiscardBufferedInputStream extends FilterInputStream {
+public class DiscardInputStream extends FilterInputStream {
 
-    private inputStream
+    private InputStream inputStream;
   
-    public DiscardBufferedInputStream(InputStream inputStream) {
+    public DiscardInputStream(InputStream inputStream) {
         this.inputStream = inputStream;
         super(this.inputStream);
     }
@@ -23,6 +23,7 @@ public class DiscardBufferedInputStream extends FilterInputStream {
     @Override
     public int read(byte[] byteArray, int offset, int length) throws IOException {
         // Read and discard bytes into the buffer, returning -1 to indicate end of stream
+        // This basically means any call to this function, regardless of the argument will return -1
         while (length > 0) {
             int bytesRead = this.inputStream.read(byteArray, offset, length);
             if (bytesRead == -1) {
@@ -32,5 +33,10 @@ public class DiscardBufferedInputStream extends FilterInputStream {
             length -= bytesRead;
         }
         return -1;
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.inputStream.close();
     }
 }
