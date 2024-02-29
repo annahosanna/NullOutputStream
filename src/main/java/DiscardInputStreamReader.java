f40ca27f18d5120e06e6f818f1bd6042240a3f6a
annahosanna <Anna.Hosanna@gmail.com>
2024-02-28 21:08:17 -0500

    Update DiscardInputStreamReader.java

--- src/main/java/DiscardInputStreamReader.java
+++ src/main/java/DiscardInputStreamReader.java
@@ -1,63 +1,74 @@
 import java.io.IOException;
-import java.io.Reader;
-    
-// Pass in something like an InputSteamReader
-// Everything throws an IOException ready (throw IOException always), 
-// mark (Throw IOException always), 
-// read (all 4 types should read everything to -1 or catch IOException and then return -1,
-// markSupported false
-// reset: IOException
-// ready: pretend so always return true
-// skip: IOException?
-// mark IOException
+import java.io.FilterReader;
 
-public class DiscardInputStreamReader extends Reader {
+public class DiscardInputStreamReader extends FilterReader {
 
     private Reader reader;
-    // int currentOffset = 0;
-    // int readLength = 1;
+    private int offset = 0;
     
     public DiscardInputStreamReader(Reader reader) {
       this.reader = reader;
+    }
+
+    @Override
+    public boolean ready() throws IOException {
+        return true;
+    }
+
+    @Override
+    public void mark(int readlimit) {
+    }
+
+    @Override
+    public boolean markSupported() {
+        return false;
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        char[] cbuf = new char[1];
+        // n is long and I am ignoring the value anyway so do not even try to loop and mod it to chunks less than the size of an int
+        int amountRead = this.read(cbuf, 0, 1);
+        return 0;
     }
     
     @Override
     public int read() throws IOException {
         char[] cbuf = new char[1];
-        return this.read(cbuf,0,1);
-    }
-
-    @Override
-    public int read(byte[] b) throws IOException {
-        char[] cbuf = new char[1];
-        return this.read(cbuf,0,1);
+        return this.read(cbuf, 0, 1);
     }
     
-    // Read from buffer
     @Override
     public int read(char[] cbuf, int off, int len) throws IOException {
         char[] buffer = new char[1024];
-        int charsRead;
+        if ( buffer == null ) {
+            throw new IOException();
+        }
         try {
-          while ((this.reader != null) && (charsRead = this.reader.read(buffer))) != -1) {
-              // Read characters from the input reader and discard them
+          while (((this.reader != null) && (this.reader.ready() == true) && ( this.reader.read(buffer,offset,1024) != -1)) {
+              offset = offset + 1024;
+              // Read characters from the input stream and discard them
               // Do nothing with the characters
           }
-          // Always return -1 to indicate end of stream (no data available)
         } catch (Exception e) {
           e.printStackTrace();
         }
+        // Always return -1 to indicate end of what is currently available in the stream (but the stream is still open)
         return -1;
     }
-
+    
     @Override
     public void close() throws IOException {
         this.reader.close();
     }
 
     @Override
-    public String toString() {
-      return "DiscardInputStreamReader";
+    public void reset() throws IOException {
     }
+    
+    @Override
+    public String toString() {
+        return "DiscardInputStreamReader";
+    } 
  
 }
